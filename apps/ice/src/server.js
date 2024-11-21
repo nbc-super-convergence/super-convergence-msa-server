b2271762985325a48c1fc51c6d24c0a8267f4b0a
MinjaeK <93378322+KonnoYuuki2@users.noreply.github.com>
2024-11-22 00:39:42 +0900

    update: 아이스 서버
    addGameSession() 추가

--- apps/ice/src/server.js
+++ apps/ice/src/server.js
@@ -1,11 +1,14 @@
-import IceServer from "./classes/models/ice.server.class.js";
+import IceServer from './classes/models/ice.server.class.js';
+import { addGameSession } from './sessions/game.session.js';
 
-const SERVER_NAME = "ice";
+const SERVER_NAME = 'ice';
 const SERVER_PORT = 5561;
 const server = new IceServer(SERVER_NAME, SERVER_PORT, [1, 2, 3, 4, 5]);
 
 await server.start();
-server.connectToDistributor("127.0.0.1", 9000, (data) => {
+await addGameSession();
+
+server.connectToDistributor('127.0.0.1', 9000, (data) => {
   // Distributor 연결
-  console.log("Distributor Notification", data);
+  console.log('Distributor Notification', data);
 });
