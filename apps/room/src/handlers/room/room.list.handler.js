91875e714eef4d453574d661b4730b049d7107a3
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-24 01:07:25 +0900

    add: 대기방 목록 조회 핸들러 구현

---
+++ apps/room/src/handlers/room/room.list.handler.js
@@ -0,0 +1,28 @@
+import { MESSAGE_TYPE } from '../../utils/constants.js';
+import roomManager from '../../classes/manager/room.manager.js';
+import { createResponse } from '../../utils/createResponse.js';
+import { getPayloadNameByMessageType } from '../index.js';
+
+export const roomListHandler = ({ socket, messageType, payload }) => {
+  try {
+    const result = roomManager.getRoomList();
+
+    // TODO: noti 구분 추가 필요
+    const packet = createResponse(result, MESSAGE_TYPE.ROOM_LIST_RESPONSE);
+
+    socket.write(packet);
+  } catch (error) {
+    console.error('[ roomListHandler ] ====>  error ', error.message, error);
+    socket.write(
+      serialize(
+        MESSAGE_TYPE.ROOM_LIST_RESPONSE,
+        {
+          success: false,
+          rooms: [],
+          failCode: 1,
+        },
+        getPayloadNameByMessageType,
+      ),
+    );
+  }
+};
