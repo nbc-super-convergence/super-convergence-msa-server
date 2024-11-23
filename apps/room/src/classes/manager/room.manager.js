84b910c23504e92c9d90dd9d4e10b18a9f0ba760
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-23 17:02:52 +0900

    add: roomManager 클래스 추가 및 구현
    room 인스턴스들을 관리할 매니저 클래스 구현

---
+++ apps/room/src/classes/manager/room.manager.js
@@ -0,0 +1,175 @@
+import Room from '../models/room.class.js';
+import { v4 as uuidv4 } from 'uuid';
+
+class RoomManager {
+  constructor() {
+    /** @type {Map<string, Room>} roomId -> Room */
+    this.rooms = new Map();
+    /** @type {Map<string, string>} userId -> roomId */
+    this.userRooms = new Map(); // 유저가 참여한 대기방
+  }
+
+  /**
+   * 대기방 생성
+   * @param {string} ownerId - 방장 ID
+   * @param {string} name - 대기방 이름
+   * @returns {RoomResponse} 생성 결과
+   */
+  createRoom(ownerId, name) {
+    const roomId = uuidv4();
+    const room = new Room(roomId, ownerId, name);
+
+    this.rooms.set(roomId, room);
+    this.userRooms.set(ownerId, roomId);
+
+    return { success: true, data: room.getRoomData(), failCode: 0 };
+  }
+
+  /**
+   * 대기방 참가
+   * @param {string} roomId - 방 ID
+   * @param {UserData} userData - 참가할 유저 정보
+   * @returns {RoomResponse} 참가 결과
+   */
+  joinRoom(roomId, userData) {
+    const room = this.rooms.get(roomId);
+    if (!room) {
+      return { success: false, data: {}, failCode: 1 };
+    }
+
+    if (room.users.size >= room.maxUsers) {
+      return { success: false, data: {}, failCode: 1 };
+    }
+
+    if (room.state !== 'wait') {
+      return { success: false, data: {}, failCode: 1 };
+    }
+
+    const result = room.joinUser(userData);
+    if (result.success) {
+      this.userRooms.set(userData.userId, roomId);
+    }
+
+    return result;
+  }
+
+  /**
+   * 대기방 퇴장
+   * @param {string} userId - 퇴장할 유저 ID
+   * @returns {RoomResponse} 퇴장 결과
+   */
+  leaveRoom(userId) {
+    const room = this.getRoomByUserId(userId);
+    if (!room) {
+      return { success: false, data: {}, failCode: 1 };
+    }
+
+    const result = room.leaveUser(userId);
+    if (result.success) {
+      this.userRooms.delete(userId);
+
+      // 방장이 나간 경우
+      if (room.ownerId === userId && room.users.size > 0) {
+        const newOwnerId = Array.from(room.users.keys())[0];
+        room.changeOwner(newOwnerId);
+      }
+
+      // 빈 방이 된 경우
+      if (room.isEmpty()) {
+        this.rooms.delete(room.id);
+      }
+    }
+
+    return result;
+  }
+
+  /**
+   * 대기방 목록 조회
+   * @returns {RoomResponse} 대기방 목록
+   */
+  getRoomList() {
+    const rooms = Array.from(this.rooms.values()).map((room) => room.getRoomData());
+
+    return {
+      success: true,
+      data: { rooms },
+      failCode: 0,
+    };
+  }
+
+  /**
+   * 유저의 준비 상태 설정
+   * @param {string} userId - 준비할 유저 ID
+   * @returns {RoomResponse} 준비 결과
+   */
+  setReady(userId) {
+    const room = this.getRoomByUserId(userId);
+    if (!room) {
+      return { success: false, data: {}, failCode: 1 };
+    }
+
+    return room.setReady(userId);
+  }
+
+  /**
+   * 대기방 상태 변경
+   * @param {string} roomId - 대기방 ID
+   * @param {string} state - 변경할 상태
+   */
+  updateRoomState(roomId, state) {
+    const room = this.rooms.get(roomId);
+    if (!room) {
+      return;
+    }
+    room.updateState(state);
+  }
+
+  /**
+   * 대기방 설정 변경
+   * @param {string} roomId
+   * @param {Object} info
+   */
+  updateRoomInfo(roomId, info) {
+    const room = this.rooms.get(roomId);
+    if (!room) {
+      return;
+    }
+
+    room.updateInfo(info);
+  }
+
+  /**
+   * 유저 ID로 대기방 조회
+   * @param {string} userId - 조회할 유저 ID
+   * @returns {Room} room - 대기방
+   */
+  getRoomByUserId(userId) {
+    const roomId = this.userRooms.get(userId);
+    if (!roomId) {
+      return;
+    }
+
+    const room = this.rooms.get(roomId);
+    if (!room) {
+      return;
+    }
+
+    return room;
+  }
+
+  /**
+   * 유저 ID로 대기방 ID 조회
+   * @param {string} userId - 조회할 유저 ID
+   * @returns {string} roomId - 대기방 ID
+   */
+  getRoomIdByUserId(userId) {
+    const roomId = this.userRooms.get(userId);
+    if (!roomId) {
+      return;
+    }
+
+    return roomId;
+  }
+}
+
+export default RoomManager;
