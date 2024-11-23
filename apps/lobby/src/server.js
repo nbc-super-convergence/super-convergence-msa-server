63f3ff273a0714ce71135a4a080d02923a7f8aa6
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-24 00:51:42 +0900

    fix: lobby/room 포트 번호 변경 및 types 추가

--- apps/lobby/src/server.js
+++ apps/lobby/src/server.js
@@ -1,11 +1,12 @@
 import LobbyServer from './classes/models/lobby.server.js';
+import { MESSAGE_TYPE } from './utils/constants.js';
 
 const SERVER_NAME = 'Lobby';
 const SERVER_PORT = 7000;
-const server = new LobbyServer(SERVER_NAME, SERVER_PORT);
+const server = new LobbyServer(SERVER_NAME, SERVER_PORT, [...MESSAGE_TYPE]);
 
 await server.start();
-server.connectToDistributor('127.0.0.1', 7777, (data) => {
+server.connectToDistributor('127.0.0.1', 9000, (data) => {
   // Distributor 연결
   console.log('Distributor Notification', data);
 });
--- apps/room/src/server.js
+++ apps/room/src/server.js
@@ -1,11 +1,12 @@
 import RoomServer from './classes/models/room.server.js';
+import { MESSAGE_TYPE } from './utils/constants.js';
 
 const SERVER_NAME = 'room';
-const SERVER_PORT = 7000;
-const server = new RoomServer(SERVER_NAME, SERVER_PORT);
+const SERVER_PORT = 7001;
+const server = new RoomServer(SERVER_NAME, SERVER_PORT, [...MESSAGE_TYPE]);
 
 await server.start();
-server.connectToDistributor('127.0.0.1', 7778, (data) => {
+server.connectToDistributor('127.0.0.1', 9000, (data) => {
   // Distributor 연결
   console.log('Distributor Notification', data);
 });
