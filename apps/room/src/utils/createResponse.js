7a7764aae7f8b6eb19110805faa76094c14762f5
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-24 01:06:09 +0900

    add: createResponse 함수 구현
    메시지 타입에 따라 결과 response 패킷을 생성하는 함수 구현

---
+++ apps/room/src/utils/createResponse.js
@@ -0,0 +1,48 @@
+import { MESSAGE_TYPE } from './constants.js';
+import { serialize } from '@repo/common/utils';
+import { getPayloadNameByMessageType } from '../handlers/index.js';
+
+export const createResponse = (result, messageType) => {
+  let response = {
+    success: result.success,
+    failCode: result.failCode,
+  };
+
+  // 메시지 타입별로 응답 데이터 구조 설정
+  switch (messageType) {
+    case MESSAGE_TYPE.ROOM_LIST_RESPONSE:
+      response = {
+        ...response,
+        rooms: result.data.rooms,
+      };
+      break;
+
+    case MESSAGE_TYPE.CREATE_ROOM_RESPONSE:
+      response = {
+        ...response,
+        room: result.data,
+      };
+      break;
+
+    case MESSAGE_TYPE.JOIN_ROOM_RESPONSE:
+      response = {
+        ...response,
+        room: result.data,
+      };
+      break;
+
+    case MESSAGE_TYPE.GAME_PREPARE_RESPONSE:
+      response = {
+        ...response,
+        isReady: result.data.isReady,
+      };
+      break;
+
+    // room 데이터가 필요없는 응답들
+    case MESSAGE_TYPE.LEAVE_ROOM_RESPONSE:
+    default:
+      break;
+  }
+
+  return serialize(messageType, response, getPayloadNameByMessageType);
+};
