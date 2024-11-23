3ef6d1517b9f7b852dd6fbf56476ee32c8f46d78
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-24 01:06:59 +0900

    add: 게임 준비 핸들러 구현

---
+++ apps/room/src/handlers/room/game.prepare.handler.js
@@ -0,0 +1,29 @@
+import { MESSAGE_TYPE } from '../../utils/constants.js';
+import roomManager from '../../classes/manager/room.manager.js';
+import { createResponse } from '../../utils/createResponse.js';
+import { getPayloadNameByMessageType } from '../index.js';
+
+export const gamePrepareRequestHandler = ({ socket, messageType, payload }) => {
+  try {
+    const { userId, isReady } = payload;
+    const result = roomManager.updateReady(userId, isReady);
+
+    // TODO: noti 구분 추가 필요
+    const packet = createResponse(result, MESSAGE_TYPE.GAME_PREPARE_RESPONSE);
+
+    socket.write(packet);
+  } catch (error) {
+    console.error('[ gamePrepareRequestHandler ] ====>  error ', error.message, error);
+    socket.write(
+      serialize(
+        MESSAGE_TYPE.GAME_PREPARE_RESPONSE,
+        {
+          success: false,
+          isReady: false,
+          failCode: 1,
+        },
+        getPayloadNameByMessageType,
+      ),
+    );
+  }
+};
