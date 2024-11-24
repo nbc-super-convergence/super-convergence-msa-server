7f9c6f688889a3adef5b8cc8a27fc4c40496d801
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-24 18:52:02 +0900

    add: 응답 패킷 생성 함수 추가 및 구현
    메시지 타입에 따라 응답 데이터 구조를 변경하도록 구현

---
+++ apps/lobby/src/utils/createResponse.js
@@ -0,0 +1,32 @@
+import { MESSAGE_TYPE } from './constants.js';
+import { serialize } from '@repo/common/utils';
+import { getPayloadNameByMessageType } from '../handlers/index.js';
+
+export const createResponse = (result, messageType) => {
+  let response = {
+    success: result.success,
+    failCode: result.failCode,
+  };
+
+  // 메시지 타입별로 응답 데이터 구조 설정
+  switch (messageType) {
+    case MESSAGE_TYPE.LOBBY_USER_LIST_RESPONSE:
+      response = {
+        ...response,
+        userList: result.data.userList,
+      };
+      break;
+
+    case MESSAGE_TYPE.LOBBY_USER_DETAIL_RESPONSE:
+      response = {
+        ...response,
+        userDetail: result.data.userDetail,
+      };
+      break;
+
+    default:
+      break;
+  }
+
+  return serialize(messageType, response, getPayloadNameByMessageType);
+};
