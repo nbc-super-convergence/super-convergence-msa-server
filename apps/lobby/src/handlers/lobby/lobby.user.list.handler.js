bc92466d6fe95756fcd666899132945f43d9f7cc
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-24 18:51:07 +0900

    add: 로비 유저 목록 조회 핸들러 추가 및 구현

---
+++ apps/lobby/src/handlers/lobby/lobby.user.list.handler.js
@@ -0,0 +1,28 @@
+import { MESSAGE_TYPE } from '../../utils/constants.js';
+import lobbyManager from '../../classes/manager/lobby.manager.js';
+import { createResponse } from '../../utils/createResponse.js';
+import { getPayloadNameByMessageType } from '../index.js';
+import { serialize } from '@repo/common/utils';
+
+export const lobbyUserListRequestHandler = ({ socket, messageType, payload }) => {
+  try {
+    const result = lobbyManager.getUserList();
+
+    const packet = createResponse(result, MESSAGE_TYPE.LOBBY_USER_LIST_RESPONSE);
+
+    socket.write(packet);
+  } catch (error) {
+    console.error('[ lobbyUserListRequestHandler ] ====>  error ', error.message, error);
+    socket.write(
+      serialize(
+        MESSAGE_TYPE.LOBBY_USER_LIST_RESPONSE,
+        {
+          success: false,
+          userList: [],
+          failCode: 1,
+        },
+        getPayloadNameByMessageType,
+      ),
+    );
+  }
+};
