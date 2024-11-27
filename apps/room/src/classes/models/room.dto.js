b0b675a44f433a489b8e04862aa2baa89dc51190
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-27 21:49:15 +0900

    add: redis와 서버의 타입 파싱을 담당하는 DTO 클래스 추가

---
+++ apps/room/src/classes/models/room.dto.js
@@ -0,0 +1,62 @@
+import { logger } from '@repo/common/config';
+
+class RoomDTO {
+  static fromRedis(redisData) {
+    try {
+      if (!redisData) return null;
+
+      return {
+        roomId: redisData.roomId,
+        ownerId: redisData.ownerId,
+        name: redisData.name,
+        lobbyId: redisData.lobbyId,
+        state: redisData.state,
+        users: new Set(JSON.parse(redisData.users)),
+        maxUser: parseInt(redisData.maxUser),
+        readyUsers: redisData.readyUsers ? new Set(JSON.parse(redisData.readyUsers)) : [],
+      };
+    } catch (error) {
+      logger.error('[ fromRedis ] ====> unknown error', error);
+    }
+  }
+
+  static toRedis(roomData) {
+    try {
+      if (!roomData) return null;
+
+      return {
+        roomId: roomData.roomId,
+        ownerId: roomData.ownerId,
+        name: roomData.name,
+        lobbyId: roomData.lobbyId,
+        state: roomData.state,
+        users: JSON.stringify(Array.from(roomData.users)),
+        maxUser: roomData.maxUser,
+        readyUsers: JSON.stringify(Array.from(roomData.readyUsers)),
+      };
+    } catch (error) {
+      logger.error('[ toRedis ] ====> unknown error', error);
+    }
+  }
+
+  static toResponse(roomData) {
+    try {
+      if (!roomData) return null;
+
+      return {
+        roomId: roomData.roomId,
+        ownerId: roomData.ownerId,
+        name: roomData.name,
+        lobbyId: roomData.lobbyId,
+        state: roomData.state,
+        users: Array.from(roomData.users),
+        maxUser: roomData.maxUser,
+        readyUsers: Array.from(roomData.readyUsers),
+      };
+    } catch (error) {
+      logger.error('[ toResponse ] ====> unknown error', error);
+    }
+  }
+}
+
+export default RoomDTO;
