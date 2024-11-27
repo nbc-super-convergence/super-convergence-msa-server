cda14eb8ab67bf43da80fd24c1816bd9f9b9aa7b
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-27 21:39:50 +0900

    refactor: lobby 클래스 수정
    단일 진실 공급을 위해 redis로만 세션을 관리
    lobby 클래스는 정적 클래스로 유틸리티 클래스로 변경

--- apps/lobby/src/classes/models/lobby.class.js
+++ apps/lobby/src/classes/models/lobby.class.js
@@ -1,84 +1,30 @@
 /**
- * @typedef {Object} UserData
- * @property {number} userId
+ * @typedef UserData
+ * @property {string} loginId
  * @property {string} nickname
  */
 
-/**
- * @typedef {Object} LobbyResponse
- * @property {boolean} success
- * @property {Object} data
- * @property {number} failCode
- */
-
 class Lobby {
-  constructor() {
-    /** @type {Map<number, UserData>} userId -> userData */
-    this.users = new Map();
+  /**
+   * 유저가 로비에 있는지 확인
+   * @param {string} lobbyId
+   * @param {string} sessionId
+   * @returns {Boolean}
+   */
+  static async isUserInLobby(lobbyId, sessionId) {
+    const currentLobbyId = await redis.getUserLocationField(sessionId, 'lobby');
+    return currentLobbyId === lobbyId;
   }
 
   /**
-   * 유저 로비 입장
-   * @param {UserData} userData - 입장할 유저의 데이터
-   * @returns {LobbyResponse}
+   * 로비 응답에 필요한 유저 데이터 형식으로 변환
+   * @param {Object} userData
+   * @returns {UserData}
    */
-  joinUser(userData) {
-    if (this.users.has(userData.userId)) {
-      return { success: false, data: {}, failCode: 1 };
-    }
-
-    this.users.set(userData.userId, userData);
-    return { success: true, data: {}, failCode: 0 };
-  }
-
-  /**
-   * 유저 로비 퇴장
-   * @param {string} userId - 퇴장할 유저 ID
-   * @returns {LobbyResponse}
-   */
-  leaveUser(userId) {
-    if (!this.users.has(userId)) {
-      return { success: false, data: {}, failCode: 1 };
-    }
-
-    this.users.delete(userId);
-    return { success: true, data: {}, failCode: 0 };
-  }
-
-  /**
-   * 유저 상세 정보 조회
-   * @param {string} targetUserId - 조회할 유저 ID
-   * @returns {LobbyResponse}
-   */
-  getUserDetail(targetUserId) {
-    const user = this.users.get(targetUserId);
-    if (!user) {
-      return { success: false, data: {}, failCode: 1 };
-    }
-
-    // TODO: 레벨이 추가되거나 없어지게되면 수정 필요
+  static formatUserData(userData) {
     return {
-      success: true,
-      data: {
-        userDetail: {
-          userId: user.userId,
-          nickname: user.nickname,
-          level: '1', // 임시
-        },
-      },
-      failCode: 0,
-    };
-  }
-
-  /**
-   * 현재 로비에 접속 중인 유저 목록
-   * @returns {UserData[]} - 접속 중인 유저들의 정보들
-   */
-  getUserList() {
-    return {
-      success: true,
-      data: { userList: Array.from(this.users.values()) },
-      failCode: 0,
+      loginId: userData.loginId,
+      nickname: userData.nickname,
     };
   }
 }
