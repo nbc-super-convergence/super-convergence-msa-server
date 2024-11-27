1b1e1643cfb0a7834d1cf13e7e7b5109afe35acb
박지영 <124862122+shd1495@users.noreply.github.com>
2024-11-27 21:36:35 +0900

    add: lobby/room 에러 처리 함수 추가 및 구현
    에러 발생 시 success - false로 보내는 함수 구현

---
+++ apps/lobby/src/utils/handle.error.js
@@ -0,0 +1,21 @@
+import { getPayloadNameByMessageType } from '../handlers/index.js';
+import { serializeForGate } from '@repo/common/utils';
+import { logger } from '@repo/common/config';
+
+export const handleError = (socket, messageType, sessionId, error) => {
+  logger.error(`[ ${messageType} ] ====>  error `, {
+    errorName: error.name,
+    error: error.message,
+    stack: error.stack,
+  });
+
+  socket.write(
+    serializeForGate(
+      messageType,
+      { success: false, failCode: 1 },
+      0,
+      getPayloadNameByMessageType(messageType),
+      [sessionId],
+    ),
+  );
+};
