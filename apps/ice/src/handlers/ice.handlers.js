8212d74bb11af8211a317976bd02544b37245877
MinjaeK <93378322+KonnoYuuki2@users.noreply.github.com>
2024-11-29 01:30:31 +0900

    update: handlers
    핸들러 로직 변경 및 game,user Manager에 handler 처리 로직 추가
    notification 수정

--- apps/ice/src/handlers/ice.handlers.js
+++ apps/ice/src/handlers/ice.handlers.js
@@ -1,17 +1,6 @@
 import iceGameManager from '../classes/managers/ice.game.manager.js';
 import iceUserManager from '../classes/managers/ice.user.manager.js';
-import {
-  iceGameReadyNotification,
-  iceGameStartNotification,
-  iceMiniGameStartNotification,
-  icePlayerDamageNotification,
-  icePlayerDeathNotification,
-  icePlayerSyncNotification,
-} from '../utils/ice.notifications.js';
-import { serializeForGate } from '@repo/common/utils';
-import { getPayloadNameByMessageType } from '@repo/common/handlers';
 import { logger } from '@repo/common/config';
-import { gameState } from '../constants/gameState.js';
 
 export const iceMiniGameStartRequestHandler = async ({ socket, payload }) => {
   try {
@@ -24,19 +13,8 @@ export const iceMiniGameStartRequestHandler = async ({ socket, payload }) => {
       throw new Error(`sessionId가 일치하는 게임이 존재하지 않습니다.`);
     }
 
-    // * 게임 상태 변경
-    game.setState(gameState.START);
-
-    // * 게임 시작 Notification
-    const sessionIds = game.getAllSessionIds();
-
-    const users = game.getAllUserBySessionId(sessionId);
-
-    const message = iceMiniGameStartNotification(users, Date.now());
-
-    const payloadType = getPayloadNameByMessageType(message.type);
-
-    const buffer = serializeForGate(message.type, message.payload, 0, payloadType, sessionIds);
+    //* iceMiniGameReadyNotification
+    const buffer = await iceGameManager.iceMiniGameReadyNoti(game);
 
     socket.write(buffer);
   } catch (error) {
@@ -44,72 +22,38 @@ export const iceMiniGameStartRequestHandler = async ({ socket, payload }) => {
   }
 };
 
-export const iceGameReadyRequestHandler = ({ socket, payload }) => {
+export const iceGameReadyRequestHandler = async ({ socket, payload }) => {
   try {
-    const { playerId } = payload;
+    const { sessionId } = payload;
 
-    // ! 세션 아이디로 유저 조회
-    const user = iceUserManager.getUserByPlayerId(playerId);
+    const user = iceUserManager.getUserBySessionId(sessionId);
 
     if (!iceUserManager.isValidUser(user)) {
       throw new Error(`유저가 존재하지 않습니다.`);
     }
 
-    user.player.isReady = true;
+    const game = iceGameManager.getGameBySessionId(user.gameId);
 
-    const game = iceGameManager.getGameBySessionId(user.sessionId);
+    // * iceGameReadyNotification
+    const readyBuffer = await iceGameManager.iceGameReadyNoti(user, game);
 
-    const sessionIds = game.getOtherSessionIds(user.id);
+    // * iceMiniGameStartNotification
+    const startBuffer = await iceGameManager.iceMiniGameStartNoti(socket, game);
 
-    const message = iceGameReadyNotification(user.player.id);
-
-    const payloadType = getPayloadNameByMessageType(message.type);
-
-    const buffer = serializeForGate(message.type, message.payload, 0, payloadType, sessionIds);
-
-    socket.write(buffer);
-
-    // ! 모든 유저가 준비가 끝났을 경우
-    if (game.getReadyCounts() === game.users.length) {
-      console.log(`모든 유저 준비 완료`);
-
-      const sessionIds = game.getAllSessionIds();
-
-      const message = iceGameStartNotification(Date.now());
-
-      const payloadType = getPayloadNameByMessageType(message.type);
-
-      const buffer = serializeForGate(message.type, message.payload, 0, payloadType, sessionIds);
-
-      socket.write(buffer);
-
-      // * 맵 변경, 게임 종료 타이머
-      game.changeMap(socket);
-      game.iceGameTimer(socket);
-    }
+    socket.write(startBuffer ? startBuffer : readyBuffer);
   } catch (error) {
     logger.error(`[iceGameReadyRequestHandler] ===> `, error);
   }
 };
 
-export const icePlayerSyncRequestHandler = ({ socket, payload }) => {
+export const icePlayerSyncRequestHandler = async ({ socket, payload }) => {
   try {
-    const { playerId, position, rotation, state } = payload;
+    const user = iceUserManager.getUserBySessionId(payload.sessionId);
 
-    const user = iceUserManager.getUserByPlayerId(playerId);
+    const game = iceGameManager.getGameBySessionId(user.gameId);
 
-    user.player.updateState(position, rotation, state);
-
-    // * 유저 상태 정보 noti
-    const game = iceGameManager.getGameBySessionId(user.sessionId);
-
-    const sessionIds = game.getOtherSessionIds(user.sessionId);
-
-    const message = icePlayerSyncNotification(user);
-
-    const payloadType = getPayloadNameByMessageType(message.type);
-
-    const buffer = serializeForGate(message.type, message.payload, 0, payloadType, sessionIds);
+    // * icePlayerSyncNotification
+    const buffer = await iceUserManager.icePlayerSyncNoti(user, game, payload);
 
     socket.write(buffer);
   } catch (error) {
@@ -117,51 +61,24 @@ export const icePlayerSyncRequestHandler = ({ socket, payload }) => {
   }
 };
 
-export const icePlayerDamageRequestHandler = ({ socket, payload }) => {
+export const icePlayerDamageRequestHandler = async ({ socket, payload }) => {
   try {
-    const { playerId } = payload;
+    const { sessionId } = payload;
 
-    const user = iceUserManager.getUserByPlayerId(playerId);
+    const user = iceUserManager.getUserBySessionId(sessionId);
 
-    const game = iceGameManager.getGameBySessionId(user.sessionId);
+    const game = iceGameManager.getGameBySessionId(user.gameId);
 
-    // 플레이어 데미지
-    user.player.damage();
+    // * icePlayerDamageNotification
+    const damageBuffer = await iceUserManager.icePlayerDamageNoti(user, game);
 
-    const sessionIds = game.getOtherSessionIds(user.sessionId);
+    // * icePlayerDeathNotification
+    const deathBuffer = await iceUserManager.icePlayerDeathNoti(user, game);
 
-    // * noti
-    const message = icePlayerDamageNotification(user.player.id);
+    socket.write(deathBuffer ? deathBuffer : damageBuffer);
 
-    const payloadType = getPayloadNameByMessageType(message.type);
-
-    const buffer = serializeForGate(message.type, message.payload, payloadType, sessionIds);
-
-    socket.write(buffer);
-
-    if (user.player.hp <= 0) {
-      user.player.playerDead();
-
-      // * 사망시 랭킹 매겨주기
-      user.player.rank = game.getAliveUser().length + 1;
-
-      const message = icePlayerDeathNotification(user);
-
-      const payloadType = getPayloadNameByMessageType(message.type);
-
-      const buffer = serializeForGate(message.type, message.payload, 0, payloadType, sessionIds);
-
-      socket.write(buffer);
-
-      // * 유저 1명 => 게임 종료
-      if (iceUserManager.getAliveUserLength() <= 1) {
-        const user = game.getAliveUser()[0];
-
-        user.player.rank = 1;
-
-        game.handleGameEnd(socket);
-      }
-    }
+    // * iceGameOverNotification
+    await iceGameManager.iceGameOver(socket, game);
   } catch (error) {
     logger.error('[icePlayerDamageRequestHandler] ===> ', error);
   }
