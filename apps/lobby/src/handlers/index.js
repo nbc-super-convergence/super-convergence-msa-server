07a994b1a84f20131f9f56dfcae92a131f9b89c3
박지영 <124862122+shd1495@users.noreply.github.com>
2024-12-03 12:34:55 +0900

    update: 로비 연결 종료 핸들러 등록

--- apps/lobby/src/handlers/index.js
+++ apps/lobby/src/handlers/index.js
@@ -1,5 +1,6 @@
 import { MESSAGE_TYPE } from '../utils/constants.js';
 import { logger } from '../utils/logger.utils.js';
+import { closeSocketRequestHandler } from './lobby/close.socket.request.handler.js';
 import { lobbyJoinRequestHandler } from './lobby/lobby.join.handler.js';
 import { lobbyLeaveRequestHandler } from './lobby/lobby.leave.handler.js';
 import { lobbyUserDetailRequestHandler } from './lobby/lobby.user.detail.handler.js';
@@ -11,6 +12,7 @@ const handlers = {
   [MESSAGE_TYPE.LOBBY_LEAVE_REQUEST]: { handler: lobbyLeaveRequestHandler },
   [MESSAGE_TYPE.LOBBY_USER_LIST_REQUEST]: { handler: lobbyUserListRequestHandler },
   [MESSAGE_TYPE.LOBBY_USER_DETAIL_REQUEST]: { handler: lobbyUserDetailRequestHandler },
+  [MESSAGE_TYPE.CLOSE_SOCKET_REQUEST]: { handler: closeSocketRequestHandler },
 };
 
 export const getHandlerByMessageType = (messageType) => {
