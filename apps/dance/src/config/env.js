1d1119d0f081085c75b89d4ed44756cee20f6075
박지영 <124862122+shd1495@users.noreply.github.com>
2024-12-09 18:16:32 +0900

    add: dance env 추가

---
+++ apps/dance/src/config/env.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+dotenv.config();
+
+export const REDIS_HOST = process.env.REDIS_HOST;
+export const REDIS_PORT = process.env.REDIS_PORT;
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+export const REDIS_USERNAME = process.env.REDIS_USERNAME;
+export const REDIS_USERPASS = process.env.REDIS_USERPASS;
+
+// SERVER
+export const SERVER_HOST = process.env.SERVER_HOST || 'localhost';
