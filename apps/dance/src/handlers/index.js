2196904eb688a102bf7b490e6e4e5c8516e0e8d5
박지영 <124862122+shd1495@users.noreply.github.com>
2024-12-09 21:46:42 +0900

    update: 핸들러 등록

--- apps/dance/src/handlers/index.js
+++ apps/dance/src/handlers/index.js
@@ -1,14 +1,21 @@
 import { MESSAGE_TYPE } from '../utils/constants.js';
 import { logger } from '../utils/logger.utils.js';
+import {
+  danceKeyPressRequest,
+  danceReadyRequestHandler,
+  danceTableCreateRequest,
+} from './dance/dance.handler.js';
 
 const handlers = {
   // * dance
-  [MESSAGE_TYPE.aa]: { handler: undefined },
+  [MESSAGE_TYPE.DANCE_READY_REQUEST]: { handler: danceReadyRequestHandler },
+  [MESSAGE_TYPE.DANCE_TABLE_CREATE_REQUEST]: { handler: danceTableCreateRequest },
+  [MESSAGE_TYPE.DANCE_KEY_PRESS_REQUEST]: { handler: danceKeyPressRequest },
 };
 
 export const getHandlerByMessageType = (messageType) => {
   if (!handlers[messageType]) {
-    logger.error(`핸들러를 찾을 수 없습니다 : messageType : ${messageType}`);
+    logger.error(`[ dance ] 핸들러를 찾을 수 없습니다 : messageType : ${messageType}`);
   }
   return handlers[messageType].handler;
 };
