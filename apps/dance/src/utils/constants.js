b7ede8d1d58b5a5a8117fa3af048c91dad64d13e
박지영 <124862122+shd1495@users.noreply.github.com>
2024-12-09 18:22:45 +0900

    add: dance 메시지 타입 추가

---
+++ apps/dance/src/utils/constants.js
@@ -0,0 +1,14 @@
+export const MESSAGE_TYPE = {
+  // dance (400 ~ 499)
+  danceMiniGameReadyNotification: 401,
+  danceReadyRequest: 402,
+  danceReadyNotification: 403,
+  danceStartNotification: 404,
+  danceTableCreateRequest: 405,
+  danceTableNotification: 406,
+  danceKeyPressRequest: 407,
+  danceKeyPressResponse: 408,
+  danceKeyPressNotification: 409,
+  danceGameOverNotification: 410,
+  danceCloseSocketNotification: 411,
+};
