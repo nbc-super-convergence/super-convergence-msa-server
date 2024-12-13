b025108379ab6e182c775b631318be79f33c7ce6
박지영 <124862122+shd1495@users.noreply.github.com>
2024-12-13 20:58:55 +0900

    update: closeSocket 방어 코드 추가

--- apps/dance/src/classes/models/dance.game.class.js
+++ apps/dance/src/classes/models/dance.game.class.js
@@ -332,6 +332,10 @@ class DanceGame extends Game {
   }
 
   isAllReady() {
+    if (this.users.size <= 0) {
+      return false;
+    }
+
     return Array.from(this.users.values()).every((user) => user.isReady);
   }
 
